d1b5eba728bed58ae754dfb7f846e5815b8cbd42
Sienna Baird <siennatanner21@gmail.com>
2025-04-28 00:35:04 -0700

    Update operation complete

--- Birthdays/ContentView.swift
+++ Birthdays/ContentView.swift
@@ -6,13 +6,11 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct ContentView: View {
-    @State private var friends: [Friend] = [
-        Friend(name: "Noelle Kang", birthday: Date(timeIntervalSince1970: 1112246400)),
-        Friend(name: "Izzy Nguyen", birthday:
-                Date(timeIntervalSince1970: 1104537600))
-    ]
+    @Query private var friends: [Friend]
+    @Environment(\.modelContext) private var context
     @State private var newName = ""
     @State private var newBirthday = Date.now
     
@@ -35,7 +33,7 @@ struct ContentView: View {
                     }
                     Button("Save") {
                         let newFriend = Friend(name: newName, birthday: newBirthday)
-                        friends.append(newFriend)
+                        context.insert(newFriend)
                         newName = ""
                         newBirthday = .now
                     }
@@ -49,4 +47,5 @@ struct ContentView: View {
 
 #Preview {
     ContentView()
+        .modelContainer(for: Friend.self, inMemory: true)
 }
