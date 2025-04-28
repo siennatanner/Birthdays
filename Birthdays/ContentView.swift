e65b466a544c4335cd978f5310280780d1b9769b
Sienna Baird <siennatanner21@gmail.com>
2025-04-28 00:29:00 -0700

    Completed create and read operations. Can add friends to list of birthdays (name and date) but doesn't save of refreshed.

--- Birthdays/ContentView.swift
+++ Birthdays/ContentView.swift
@@ -8,14 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var friends: [Friend] = [
+        Friend(name: "Noelle Kang", birthday: Date(timeIntervalSince1970: 1112246400)),
+        Friend(name: "Izzy Nguyen", birthday:
+                Date(timeIntervalSince1970: 1104537600))
+    ]
+    @State private var newName = ""
+    @State private var newBirthday = Date.now
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List(friends, id: \.name) { friend in
+                HStack {
+                    Text(friend.name)
+                    Spacer()
+                    Text(friend.birthday, format: .dateTime.month(.wide).day().year())
+                }
+            }
+            .navigationTitle("Birthdays")
+            .safeAreaInset(edge: .bottom) {
+                VStack(alignment: .center, spacing: 20) {
+                    Text("New Birthday")
+                    DatePicker(selection: $newBirthday, in: Date.distantPast...Date.now, displayedComponents: .date) {
+                        TextField("Name", text: $newName)
+                            .textFieldStyle(.roundedBorder)
+                    }
+                    Button("Save") {
+                        let newFriend = Friend(name: newName, birthday: newBirthday)
+                        friends.append(newFriend)
+                        newName = ""
+                        newBirthday = .now
+                    }
+                }
+                .padding()
+                .background(.bar)
+            }
         }
-        .padding()
     }
 }
 
---
+++ Friend.swift
@@ -0,0 +1,18 @@
+//
+//  Friend.swift
+//  Birthdays
+//
+//  Created by Sienna Tanner on 4/28/25.
+//
+
+import Foundation
+
+class Friend {
+    var name: String
+    var birthday: Date
+    
+    init(name: String, birthday: Date) {
+        self.name = name
+        self.birthday = birthday
+    }
+}
